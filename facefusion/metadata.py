ab4542dced72a52940727255f1c0c6b1a2ab52b6
e.nabati <erfard26@gmail.com>
2024-07-12 02:52:18 +0400

    test

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -2,17 +2,10 @@ METADATA =\
 {
 	'name': 'V-TURN Demo',
 	'description': 'Next generation face swapper and enhancer',
-<<<<<<< HEAD
 	'version': '0.3.0',
 	'license': 'V-TURN',
 	'author': '',
 	'url': 'https://vturn.ir/eng/'
-=======
-	'version': '2.6.1',
-	'license': 'MIT',
-	'author': 'Henry Ruhs',
-	'url': 'https://facefusion.io'
->>>>>>> 57016d7c778c70f58fc54c49013d57179874a505
 }
 
 
